67ae9b6ddcba18ef5903afc5998f16f84364fa6e
nikit <nikitatayatsko308@gmail.com>
2025-12-21 21:38:48 +0300

    add docker-compose.yml

--- src/main/java/com/payment/service/config/LiquibaseMongoConfig.java
+++ src/main/java/com/payment/service/config/LiquibaseMongoConfig.java
@@ -7,6 +7,7 @@ import liquibase.exception.DatabaseException;
 import liquibase.exception.LiquibaseException;
 import liquibase.ext.mongodb.database.MongoLiquibaseDatabase;
 import liquibase.resource.ClassLoaderResourceAccessor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,17 +15,22 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class LiquibaseMongoConfig {
 
+    @Value("${spring.liquibase.url}")
+    private String liquibaseUrl;
+
+    @Value("${spring.liquibase.change-log}")
+    private String changeLog;
+
     @Bean
     public CommandLineRunner runLiquibase() {
         return args -> {
             Database database = null;
             try {
-                String url = "mongodb://localhost:27017/payment_db";
-                database = DatabaseFactory.getInstance().openDatabase(url, null, null, null, null);
+                database = DatabaseFactory.getInstance().openDatabase(liquibaseUrl, null, null, null, null);
                 if (!(database instanceof MongoLiquibaseDatabase)) {
                     throw new RuntimeException("Database is not a MongoDB instance");
                 }
-                Liquibase liquibase = new Liquibase("db.changelog/db.changelog-payment.yaml", new ClassLoaderResourceAccessor(), database);
+                Liquibase liquibase = new Liquibase(changeLog, new ClassLoaderResourceAccessor(), database);
                 liquibase.update("");
                 System.out.println("Liquibase migration applied successfully.");
             } catch (LiquibaseException e) {
