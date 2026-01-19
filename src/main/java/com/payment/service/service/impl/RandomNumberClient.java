c38c9ef489818e1a7421fb96397a7a8fce8df82f
nikit <nikitatayatsko308@gmail.com>
2026-01-20 00:42:08 +0300

    fix

--- src/main/java/com/payment/service/service/impl/RandomNumberClient.java
+++ src/main/java/com/payment/service/service/impl/RandomNumberClient.java
@@ -1,27 +1,25 @@
 package com.payment.service.service.impl;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.client.RestTemplate;
+
 
 import java.util.List;
 
 @Component
 public class RandomNumberClient {
 
-    private final WebClient webClient;
+    private final RestTemplate restTemplate;
+    private final String baseUrl;
 
-    public RandomNumberClient(WebClient.Builder builder,
-                           @Value("${spring.external.random.base_url}") String baseUrl) {
-        this.webClient = builder.baseUrl(baseUrl).build();
+    public RandomNumberClient(@Value("${spring.external.random.base_url}") String baseUrl) {
+        this.restTemplate = new RestTemplate();
+        this.baseUrl = baseUrl;
     }
 
     public List<Integer> getRandomNumber() {
-        return webClient.get()
-                .accept(MediaType.APPLICATION_JSON)
-                .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<List<Integer>>() {})
-                .block();
+        ResponseEntity<List> response = restTemplate.getForEntity(baseUrl, List.class);
+        return response.getBody();
     }
 }
